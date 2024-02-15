cf91f7020062def4831b5fe22748e4d656a56eee
martin <martin.alvarado2002@gmail.com>
2024-02-15 14:43:15 +0100

    [11-146]: Using PM2 To Create Clusters

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cluster = require('cluster');
-const os = require('os');
+cluster.schedulingPolicy = cluster.SCHED_RR;
 
 const app = express();
 
@@ -21,17 +21,7 @@ app.get('/timer', (req, res) => {
 });
 
 console.log(`🔎 | SERVER | Runing server.js...`);
-if (cluster.isMaster) {
-  console.log(`🔎 | SERVER | Master has started...`);
-  const NUM_WORKERS = os.cpus().length;
-  console.log(`🔎 | SERVER | NUM_WORKERS: `, NUM_WORKERS);
-
-  for (let i = 0; i < NUM_WORKERS; i++) {
-    cluster.fork();
-  }
-} else {
-  console.log(`🔎 | SERVER | Worker process has started...`);
-  app.listen(3000, () => {
-    console.log(`🔎 | SERVER | listening to port 3000...`);
-  });
-}
+console.log(`🔎 | SERVER | Worker process has started.`);
+app.listen(3000, () => {
+  console.log(`🔎 | SERVER | listening to port 3000...`);
+});
